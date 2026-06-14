7ad53fd2fc6296591b0190dca05c4c4d998de5dd
farhans25-coding <farhan.s25@mhs.usk.ac.id>
2026-06-14 20:18:38 +0700

    Update output.h

--- output.h
+++ output.h
@@ -1,1 +1,18 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
 
+#include "bag_of_words.h"
+
+void saveToFile(
+    const char *filename,
+    WordData data[],
+    int n,
+    double elapsed_ms);
+
+void displayTopK(
+    WordData data[],
+    int n,
+    int k,
+    double elapsed_ms);
+
+#endif
