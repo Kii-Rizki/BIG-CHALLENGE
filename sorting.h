e9c29000f9663611b6ae5b706094715bdfd0f3c0
farhans25-coding <farhan.s25@mhs.usk.ac.id>
2026-06-14 20:16:59 +0700

    Update sorting.h

--- sorting.h
+++ sorting.h
@@ -1,1 +1,10 @@
+#ifndef SORTING_H
+#define SORTING_H
 
+#include "bag_of_words.h"
+
+void insertionSort(WordData arr[], int n);
+void quickSort(WordData arr[], int low, int high);
+void heapSort(WordData arr[], int n);
+
+#endif
